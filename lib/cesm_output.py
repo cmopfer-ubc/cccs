efc1f430abadb13f986a3b1b27db6fbffb5be823
cmopfer-ubc <cmopfer@student.ubc.ca>
2026-04-01 17:31:32 -0700

    CESM output query pt2

--- lib/cesm_output.py
+++ lib/cesm_output.py
@@ -4,8 +4,11 @@ Created: Camden Opfer, March 2026
 A collection of scripts to do basic analysis of CESM output
 """
 import os
+import glob
+import re
 import numpy as np
 import netCDF4 as nc
+from .utils import log
 
 def cam_annual_mean(camRoot:str, variable:str = 'lev', year:str = '0001', subset:slice|None = None) -> np.ndarray:
     """
@@ -51,8 +54,75 @@ def cam_annual_mean(camRoot:str, variable:str = 'lev', year:str = '0001', subset
 
 def query(outputPath:str, archive:bool = True, searchTerm:str|None = None, returnPath:str|None = None):
     """
-    Identifies the different types on netCDF files (e.g. <run-name>.cam.h1 or <run-name>.clm2.h0) within the output path, searching through <component>/hist subdirectories if this is the path to an archive directory (rather than a directory that immediately contains the output). If a search term is provided, will return a list of files/variables containing that term (if any exist). If return path is specified, the output of this function is saved to a text file.
+    Identifies the different types of netCDF files (e.g. <run-name>.cam.h1 or <run-name>.clm2.h0) within the output path, searching through <component>/hist subdirectories if this is the path to an archive directory. If a search term is provided, will return a list of files/variables containing that term (if any exist). If return path is specified, the output of this function is saved to a text file.
+    
+    :param outputPath: The root directory for the CESM run's output.
+    :type outputPath: str
+    :param archive: Whether this is an archive directory (query will search for outputPath/<component>/hist/*.nc files) or not (query will search for outputPath/*.nc files)
+    :type archive: bool, optional
+    :param searchTerm: A string (can be a regex string) which the variable names and descriptions of all relevant output files will be searched for. Default is None, so all files/variables are returned. Still helpful because they are neatly organized.
+    :type searchTerm: str or None, optional
+    :param returnPath: Path to a text file to which the output of this function will be written. Default is None, in which case cccs.utils.log will either print or log the output.
+    :type returnPath: str or None, optional
     """
+    def queryOutput(output, returnFile = returnPath):
+        """
+        Either logs the output of query() or saves it to the specified path.
+        """
+        if returnFile is None:
+            log(output)
+        else:
+            try:
+                os.makedirs(returnPath, exist_ok=True)
+                with open(returnFile, 'a', encoding='UTF-8') as f:
+                    f.write('\n' + output)
+            except Exception as e:
+                log(f'Saving queryOutput to file had error: {e}\nWill log output instead.')
+                log(output)
+
+    # Get list of all the potentially relevant files
+    if archive:
+        allFiles = glob.glob(os.path.join(outputPath, '*', 'hist', '*.nc'), recursive=True) # Assumes outputPath/<component>/hist/<fname>.nc structure
+    else:
+        allFiles = glob.glob('*.nc', root_dir=outputPath) # Assumes outputPath/<fname>.nc structure
+
+    allFiles.sort()
+
+    # Find files with unique forms. E.g. <run-name>.cam.h0.stuff and <run-name>.cam.h1.things are different kinds of file, but not <run-name>.cam.h0.stuff and <run-name>.cam.h0.blah
+    fileTypes = {}
+    for file in allFiles:
+        fname = os.path.basename(file) # Get the bit after the last / (or \ if on Windows for some reason)
+        ftype = '.'.join(fname.split('.')[-4:-2])
+        try:
+            fileTypes[ftype].append(file)
+        except KeyError:
+            fileTypes[ftype] = [file]
+
+    queryOutput(f'Found {len(fileTypes)} distinct file types.')
+
+    if searchTerm:
+        hits = []
+        for files in fileTypes.values():
+            ds = nc.Dataset(files[0], 'r')
+            varsDict = ds.variables
+
+            for varName, varDescription in varsDict:
+                if re.search(searchTerm, varName) or re.search(searchTerm, varDescription):
+                    hits.append(files, varName, varDescription)
+
+        if hits:
+            queryOutput(f'Found {len(hits)} types of files satisfying the search term. They are...')
+            for hit in hits:
+                queryOutput(f'\tFiles:\n{hit[0]}\n\tVariable name: {hit[1]}\n\tVariable details:\n{hit[2]}')
+        else:
+            queryOutput(f'No output files matching search term {searchTerm} found in {outputPath}.')
+
+    else:
+        for files in fileTypes.values():
+            ds = nc.Dataset(files[0], 'r')
+            varsDict = ds.variables
+
+            queryOutput(f'The files:\n{files}\nContain the variables:\n{varsDict}\n')
 
 if __name__ == '__main__':
     camDir = '/home/cmopfer/scratch/cesm/output/archive/NdgParams_Ctrl/atm/hist'
